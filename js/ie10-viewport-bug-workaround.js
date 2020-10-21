7448ee18b287ceb7d8742a484af4fa776518b385
Divinux <Divinxu@googlemail.com>
2020-10-21 20:28:18 +0200

    Wait no hang on... ...now

--- js/ie10-viewport-bug-workaround.js
+++ js/ie10-viewport-bug-workaround.js
@@ -21,3 +21,16 @@
   }
 
 })();
+
+function centerModal() {
+    $(this).css('display', 'block');
+    var $dialog = $(this).find(".modal-dialog");
+    var offset = ($(window).height() - $dialog.height()) / 3;
+    // Center modal vertically in window
+    $dialog.css("margin-top", offset);
+}
+
+$('.modal').on('show.bs.modal', centerModal);
+$(window).on("resize", function () {
+    $('.modal:visible').each(centerModal);
+});
